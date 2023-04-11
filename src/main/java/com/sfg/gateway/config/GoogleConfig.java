f20277757909d414adcfb9b28b6ea4dfd669e168
Michael Ortiz <12751082+Odysseuss@users.noreply.github.com>
2023-04-10 20:26:09 -0400

    removing bean status for the google config

--- src/main/java/com/sfg/gateway/config/GoogleConfig.java
+++ src/main/java/com/sfg/gateway/config/GoogleConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 public class GoogleConfig {
 
-    @Bean
+//    @Bean
     public RouteLocator googleConfig(RouteLocatorBuilder builder) {
         return builder
                 .routes()
